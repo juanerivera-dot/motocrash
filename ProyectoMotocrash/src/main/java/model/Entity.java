011f0de50aa8bbc7c3c99f70c303ab92a78189a8
juanitagiraldo-ship-it <juanita.giraldo@autonoma.edu.co>
2026-05-14 12:46:19 -0500

    Imágenes del juego.

--- ProyectoMotocrash/src/main/java/model/Entity.java
+++ ProyectoMotocrash/src/main/java/model/Entity.java
@@ -1,8 +1,26 @@
 package model;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Clase abstracta base para todas las entitades del juego.
- * Define posición, sprite
+ * Define posición, sprite, dirección y posición.
+ * Implementó el método actualizar().
  */
-public class Entity {
+public abstract class Entity {
+    /**
+     *  Constantes manejo direccióm entidad.
+     */
+    public static final int DIR_NONE = 0;
+    public static final int DIR_LEFT = 1;
+    public static final int DIR_RIGHT = 2;
+
+    // Atributos.
+    private int x;
+    private int y;
+    private int widht;
+    private int height;
+    private BufferedImage sprite;
+    private boolean active;
+    private int direction;
 }
